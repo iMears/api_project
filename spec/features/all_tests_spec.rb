4970331b5cde4b98f01deed1ada977c59bcd7419
iMears <maxwellmears@gmail.com>
2015-01-30 10:58:34 -0800

    Added rspec tests

--- spec/features/all_tests_spec.rb
+++ spec/features/all_tests_spec.rb
@@ -8,9 +8,39 @@ describe 'GET /'  do
 end
 
 describe 'POST /users' do
-  context 'when given valid perameters' do
+  let!(:test_user){{input: {first_name: "max", last_name: "mears", city: "Santa Cruz", state: "CA", email: "email@email.com", phone_number: "7757426305", password: "kldsfasdfa"}}}
+  let!(:false_test_user){{input: {first_name: "max"}}}
+
+  before :each do
     User.destroy_all
-    post '/users', {first_name: "max", last_name: "mears", city: "Santa Cruz", state: "CA", email: "email", phone_number: "7757426305", password_hash: "738wii3990f"}
-      expect(Gadget.all.size).to be(1)
   end
+
+  context 'when given valid perameters' do
+    it "creates a new user" do
+      post '/users', test_user
+      expect(User.count).to eq(1)
+    end
+
+    it 'redirects the user to the /user/:email route' do
+      post '/users', test_user
+      expect(last_response).to be_redirect
+    end
+  end
+
+  context 'when given invalid perameters' do
+    it 'does not create a new user' do
+      post '/users', false_test_user
+      expect(User.count).to eq(0)
+    end
+  end
+
+  context 'when the requres does not include a unique name value' do
+    it 'does not create a new user' do
+      post 'users', test_user
+      post 'users', test_user
+      expect(User.count).to eq(1)
+    end
+  end
+
+
 end
